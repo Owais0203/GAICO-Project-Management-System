e1de89874f6997dbaf0bd535a82f37d92d444dfb
muhammad.suhail <muhammad.suhail@atacama-blooms>
2025-03-25 18:19:47 +0100

    feat: Added the different models business logic.

---
+++ src/main/java/com/java/springBoot/backend/Model/Message.java
@@ -0,0 +1,29 @@
+package com.java.springBoot.backend.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String content;
+
+    private LocalDate createdAt;
+
+    @ManyToOne
+    private Chat chat;
+
+    @ManyToOne
+    private Users sender;
+}
