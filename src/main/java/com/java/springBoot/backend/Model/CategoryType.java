d391f81c40bfd0bc3eea87cfac4cbcaaceb54ba3
muhammad.suhail <muhammad.suhail@atacama-blooms>
2025-04-28 17:42:49 +0200

    fix: an issue.

---
+++ src/main/java/com/java/springBoot/backend/Model/CategoryType.java
@@ -0,0 +1,5 @@
+package com.java.springBoot.backend.Model;
+
+public enum CategoryType {
+    NOT_STARTED, IN_PROGRESS, COMPLETED, CLOSED, CANCELLED
+}
--- src/main/java/com/java/springBoot/backend/Model/Project.java
+++ src/main/java/com/java/springBoot/backend/Model/Project.java
@@ -22,7 +22,7 @@ public class Project {
 
     private String name;
     private String description;
-    private String category;
+    private CategoryType category;
 
     private LocalDateTime startDate;
     private LocalDateTime endDate;
